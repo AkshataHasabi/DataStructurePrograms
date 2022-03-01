d5ec7858e3b12f9cab5fd31e5301f9826e4298eb
AkshataHasabi <akshuh818@gmail.com>
2022-03-01 19:35:13 +0530

    ADDED:finding the frequency of a sentence using hashtable

--- HashMain.java
+++ HashMain.java
@@ -4,5 +4,12 @@ package com.bridgelab.hashtable;
 public class HashMain {
 	public static void main(String[] args) {
 		System.out.println("welcome to hashtable programms");
+		HashMapService HashMap = new HashMapService();
+		String givenString = "To be or not to be";
+		String[] myStr = givenString.toLowerCase().split(" ");
+		for (String string : myStr){
+			HashMap.put(string, 1);
+		}
+		HashMap.print();
 	}
 }
---
+++ Node.java
@@ -0,0 +1,12 @@
+package com.bridgelab.hashtable;
+
+public class Node {
+	String key;
+	int value;
+	Node next;
+	public Node() {
+		super();
+	}	
+}
+
+
