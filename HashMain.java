4559731fe257ab571d88f7afb8835f7507203c19
AkshataHasabi <akshuh818@gmail.com>
2022-03-01 19:52:58 +0530

    UPDATED:finding the frequency of a paragraph phrase using hashtable

--- HashMain.java
+++ HashMain.java
@@ -4,12 +4,22 @@ package com.bridgelab.hashtable;
 public class HashMain {
 	public static void main(String[] args) {
 		System.out.println("welcome to hashtable programms");
-		HashMapService HashMap = new HashMapService();
+		
+		HashMapService HashMap1 = new HashMapService();
 		String givenString = "To be or not to be";
 		String[] myStr = givenString.toLowerCase().split(" ");
 		for (String string : myStr){
-			HashMap.put(string, 1);
+			HashMap1.put(string, 1);
 		}
-		HashMap.print();
+		HashMap1.print();
+		
+		HashMapService hashMap2 = new HashMapService();
+		String givenParagraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		String[] myStr2 = givenParagraph.toLowerCase().split(" ");
+		for (String string : myStr2) 
+		{
+			hashMap2.put(string, 1);
+		}
+		hashMap2.print();
 	}
 }
